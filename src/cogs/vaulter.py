3a557c540d13baa9723c5b526ee2ae306b11f201
SirSkye <?>
2024-11-17 22:39:17 -0500

    Experimenting with !store

--- src/cogs/vaulter.py
+++ src/cogs/vaulter.py
@@ -1,8 +1,12 @@
 from discord.ext import commands
+import discord
 import logging
+import data
+import os
 
 class Vaulter(commands.Cog):
     def __init__(self, bot:commands.Bot):
+        self.data = data.Data(os.getenv("DATA_PATH"))
         self.bot = bot
         self.logger = logging.getLogger("discord.vaulter")
     
@@ -13,10 +17,10 @@ class Vaulter(commands.Cog):
         await ctx.send(f'Pong! Latency: {round(self.bot.latency * 1000)}ms')
     
     @commands.command()
-    async def hello(self, ctx):
+    async def store(self, ctx, *args):
         """Says hello to the user"""
-        self.logger.info(f"Hello command used by {ctx.author}")
-        await ctx.send(f"Hello {ctx.author.name}!")
+        self.logger.info(f"Store used by {ctx.author}")
+        await ctx.send(file=discord.File(r"D:\projects\pypystuff\bpVault\temp\e.txt"))
 
 async def setup(bot:commands.Bot):
     await bot.add_cog(Vaulter(bot))
--- src/data.py
+++ src/data.py
@@ -1,3 +1,3 @@
 class Data:
-    def __init__(self):
-        pass
+    def __init__(self, data_path: str):
+        self.path = data_path
